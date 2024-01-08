5ea717baca0e14eb35d1e8dbb8ee22d25ba2b071
Jamie Briggs <jbrig@nhsbsa.nhs.uk>
2024-01-08 00:08:00 +0000

    ExpandShipComponent.cs MonoBehaviour added and started

---
+++ ShipExpander/Core/SELogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace ShipExpander.Core;
+
+public abstract class SELogger
+{
+    public static void Log(String message, LogLevel logLevel = LogLevel.Info)
+    {
+        ShipExpanderModBase.Log.Log(logLevel, message);
+    }
+
+    public static void Log(String source, String message, LogLevel logLevel = LogLevel.Info)
+    {
+        Log($"({source}) -> {message}", logLevel);
+    }
+
+    public static void Log(GameObject gameObject, String message, LogLevel logLevel = LogLevel.Info)
+    {
+        Log(gameObject.name, message, logLevel);
+    }
+}
---
+++ ShipExpander/Helper/GameObjectHelper.cs
@@ -0,0 +1,21 @@
+﻿using ShipExpander.Core;
+using UnityEngine;
+using LogLevel = BepInEx.Logging.LogLevel;
+
+namespace ShipExpander.Helper;
+
+public abstract class GameObjectHelper
+{
+    public static GameObject FindObjectByName(string name)
+    {
+        var gameObject = GameObject.Find(name);
+        if (gameObject != null)
+        {
+            SELogger.Log($"Found {name} {gameObject.transform}");
+            return gameObject;
+        }
+
+        SELogger.Log($"Could not find {name}", LogLevel.Error);
+        throw new ShipExpanderException($"Could not find GameObject with name: {name}");
+    }
+}
