9af77fe65d0ac86debc2d4efbc7629a0345267c1
Jamie Briggs <jbrig@nhsbsa.nhs.uk>
2024-01-19 19:29:43 +0000

    Started on render plane

---
+++ ShipExpander/Helper/UnityBundleHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx.Logging;
+using ShipExpander.Core;
+using UnityEngine;
+
+namespace ShipExpander.Helper;
+
+public abstract class UnityBundleHelper
+{
+    public static AssetBundle LoadResource(string unityPackageName)
+    {
+        // Load assets
+        var sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var output = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, unityPackageName));
+        if (output == null) {
+            SELogger.Log("UnityBundleHelper", "Failed to load custom assets.", LogLevel.Error); 
+        }
+
+        return output;
+    }
+}
