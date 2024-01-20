8ec0d5354246a5a44a337981b93d51f05cc43bf4
Jamie Briggs <jbrig@nhsbsa.nhs.uk>
2024-01-20 16:35:18 +0000

    Updated configuration

--- ShipExpander/Core/SELogger.cs
+++ ShipExpander/Core/SELogger.cs
@@ -6,17 +6,17 @@ namespace ShipExpander.Core;
 
 public abstract class SELogger
 {
-    public static void Log(String message, LogLevel logLevel = LogLevel.Debug)
+    public static void Log(String message, LogLevel logLevel = LogLevel.Message)
     {
         ShipExpanderModBase.Log.Log(logLevel, message);
     }
 
-    public static void Log(String source, String message, LogLevel logLevel = LogLevel.Debug)
+    public static void Log(String source, String message, LogLevel logLevel = LogLevel.Message)
     {
         Log($"({source}) -> {message}", logLevel);
     }
 
-    public static void Log(GameObject gameObject, String message, LogLevel logLevel = LogLevel.Debug)
+    public static void Log(GameObject gameObject, String message, LogLevel logLevel = LogLevel.Message)
     {
         Log(gameObject.name, message, logLevel);
     }
