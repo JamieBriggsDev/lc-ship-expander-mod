d2059de74eed196b676e41df34bb52b156d5d3b4
Jamie Briggs <jbrig@nhsbsa.nhs.uk>
2024-01-27 01:09:49 +0000

    Changes
    Signed-off-by: Jamie Briggs <jbrig@nhsbsa.nhs.uk>

--- createPublishFolder.ps1
+++ createPublishFolder.ps1
@@ -9,8 +9,8 @@ New-Item -Path "$PUBLISH_DIR" -ItemType "directory"
 
 Write-Output "2:    Copying DLL"
 
-Copy-Item -Path "bin\$config\netstandard2.1\JBriggs.ShipExpander.dll" -Destination "$PUBLISH_DIR"
+Copy-Item -Path "ShipExpander\bin\$config\netstandard2.1\JBriggs.ShipExpander.dll" -Destination "$PUBLISH_DIR"
 
 Write-Output "3:    Copying Assets"
 
-Copy-Item -Path "AssetBundles" -Destination "$PUBLISH_DIR" -Recurse
+Copy-Item -Path "ShipExpander\AssetBundles" -Destination "$PUBLISH_DIR\AssetBundles" -Recurse
