34514fe26b32982e2cd04975ffbe8738250c3d58
Jamie Briggs <jbrig@nhsbsa.nhs.uk>
2024-01-27 00:37:31 +0000

    Changes to createPublishFolder.ps1
    Signed-off-by: Jamie Briggs <jbrig@nhsbsa.nhs.uk>

--- ShipExpander/tools/createPublishFolder.ps1
+++ ShipExpander/tools/createPublishFolder.ps1
@@ -1,16 +1,16 @@
-﻿param ($config)
+﻿#param ($config)
 
-$PUBLISH_DIR = "bin\publish-$config"
+#$PUBLISH_DIR = "bin\publish-$config"
 
 Write-Output "1:    Creating initial folder structure"
 
-New-Item -Path "$PUBLISH_DIR" -ItemType "directory"
+#New-Item -Path "$PUBLISH_DIR" -ItemType "directory"
 
 
-Write-Output "2:    Copying DLL"
+#Write-Output "2:    Copying DLL"
 
-Copy-Item -Path "bin\$config\netstandard2.1\JBriggs.ShipExpander.dll" -Destination "$PUBLISH_DIR"
+#Copy-Item -Path "bin\$config\netstandard2.1\JBriggs.ShipExpander.dll" -Destination "$PUBLISH_DIR"
 
-Write-Output "3:    Copying Assets"
+#Write-Output "3:    Copying Assets"
 
-Copy-Item -Path "AssetBundles" -Destination "$PUBLISH_DIR" -Recurse
+#Copy-Item -Path "AssetBundles" -Destination "$PUBLISH_DIR" -Recurse
