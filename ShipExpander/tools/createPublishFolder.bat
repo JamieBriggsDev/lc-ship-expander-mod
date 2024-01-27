2ac9947e62d1aea500f3e558fd05ffc6406ef882
Jamie Briggs <jbrig@nhsbsa.nhs.uk>
2024-01-27 00:34:11 +0000

    Changes to build folder to publish
    Signed-off-by: Jamie Briggs <jbrig@nhsbsa.nhs.uk>

---
+++ ShipExpander/tools/createPublishFolder.bat
@@ -0,0 +1,1 @@
+Powershell.exe -ExecutionPolicy Unrestricted -file ".\tools\createPublishFolder.ps1" -config %1
---
+++ ShipExpander/tools/createPublishFolder.ps1
@@ -0,0 +1,16 @@
+﻿param ($config)
+
+$PUBLISH_DIR = "bin\publish-$config"
+
+Write-Output "1:    Creating initial folder structure"
+
+New-Item -Path "$PUBLISH_DIR" -ItemType "directory"
+
+
+Write-Output "2:    Copying DLL"
+
+Copy-Item -Path "bin\$config\netstandard2.1\JBriggs.ShipExpander.dll" -Destination "$PUBLISH_DIR"
+
+Write-Output "3:    Copying Assets"
+
+Copy-Item -Path "AssetBundles" -Destination "$PUBLISH_DIR" -Recurse
