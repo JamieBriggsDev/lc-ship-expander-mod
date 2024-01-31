1a633c3c41baa37deb0ae29444bec4147dd81e4b
Jamie Briggs <personal@jbriggs.dev>
2024-01-31 22:37:44 +0000

    .gitignore is now working

--- ShipExpander/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ShipExpander
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "JBriggs.ShipExpander";
-        public const string PLUGIN_NAME = "Ship Expander";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
