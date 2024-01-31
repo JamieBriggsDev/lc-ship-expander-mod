0d4f1f04cecda88ad9c0458d0f27132392a855ba
Jamie Briggs <personal@jbriggs.dev>
2024-01-31 22:37:13 +0000

    Develop thunderstore GitHub actions (#11)

---
+++ createPublishFolder.sh
@@ -0,0 +1,13 @@
+﻿CONFIG="$1"
+PUBLISH_DIR="bin\publish-$CONFIG"
+
+echo "$PUBLISH_DIR"
+
+echo '1:    Creating initial folder structure'
+mkdir "$PUBLISH_DIR"
+
+echo '2:    Copying DLL'
+cp "bin\$CONFIG\netstandard2.1\JBriggs.ShipExpander.dll" "$PUBLISH_DIR"
+
+echo '3:    Copying Assets'
+cp -r ./AssetBundles "$PUBLISH_DIR"
